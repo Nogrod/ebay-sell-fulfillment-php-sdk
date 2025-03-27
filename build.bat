cce2f0bd557eb923306444e5ba08e618dc46b6d8
Nogrod <strikeomania@gmail.com>
2025-03-27 15:33:56 +0100

    cleanup

--- build.bat
+++
@@ -1,2 +0,0 @@
-rem openapi request body missing
-java -jar openapi-generator-cli.jar generate -i sell_fulfillment_v1_oas3.yaml -g php-nextgen --git-user-id nogrod --git-repo-id ebay-sell-fulfillment-php-sdk --artifact-version 0.0.1 --enable-post-process-file --additional-properties invokerPackage=eBay\Sell\Fulfillment
